cc3f2d87b3b68948e5c305b13400a94e6ae45e26
oliver <qiwei@ts-taxi.com>
2019-01-24 16:41:46 +0800

    关闭页面停止秒表

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,43 +4,40 @@ Page({
     btnText: "开始",
     // 定时器的编号
     stopwatchNumber: "",
-    seconds:1,
-    btnBg:"lightgreen",
-    showbtnReset:true
+    seconds: 1,
+    btnBg: "lightgreen",
+    showbtnReset: true
   },
-
 
   // 开始计时，或停止计时
   startTime: function() {
-      var context = this;
+    var context = this;
 
     if (context.data.btnText == "开始" || context.data.btnText == "继续") {
-        context.data.stopwatchNumber = setInterval(function() {
-          var sec = context.data.seconds++;
-          var format = require("../../utils/util.js");
-
-          context.setData({
-            time: format.sec_to_time(sec)
-          })
-
-        }, 1000);
-        context.setData({
-          btnText: "停止",
-          btnBg:"red",
-          showbtnReset:false
-        })
-
-      } else {
-        clearInterval(context.data.stopwatchNumber);
+      context.data.stopwatchNumber = setInterval(function() {
+        var sec = context.data.seconds++;
+        var format = require("../../utils/util.js");
 
         context.setData({
-          btnText: "继续",
-          btnBg: "lightgreen"
+          time: format.sec_to_time(sec)
         })
-      }
+
+      }, 1000);
+      context.setData({
+        btnText: "停止",
+        btnBg: "red",
+        showbtnReset: false
+      })
+
+    } else {
+      clearInterval(context.data.stopwatchNumber);
+
+      context.setData({
+        btnText: "继续",
+        btnBg: "lightgreen"
+      })
     }
-
-    ,
+  },
   // 重置
   reset: function() {
     clearInterval(this.data.stopwatchNumber);
@@ -51,7 +48,9 @@ Page({
       btnBg: "lightgreen",
       showbtnReset: true
     })
+  },
 
-  }
-
+  onHide: function() {
+    clearInterval(this.stopwatchNumber);
+  },
 })
